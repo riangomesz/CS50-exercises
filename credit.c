205459fc9f9904eb51f616fac2ca56f2af67fc74
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2023-01-17 11:56:35 -0300

    Update credit.c

--- credit.c
+++ credit.c
@@ -1,5 +1,4 @@
-/* Algoritmo que a partir de seus cálculos diz ao usuário que o número digitado é do cartão "MasterCard" ou "AmericaExpress" com base na lógica escrita na linguagem C*/
-
+/* Algoritmo que a partir de seus cálculos diz ao usuário se o número digitado é do cartão "MasterCard" ou "AmericaExpress" com base na lógica escrita na linguagem C*
 #include <stdio.h>
 #include <cs50.h>
 
