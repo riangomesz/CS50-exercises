9d60eaf600d9c6d55336fa9010b5dc677e93546e
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2023-01-24 15:00:13 -0300

    Add files via upload

---
+++ test_score.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+const int TOTAL = 3;
+
+int main(void)
+{
+    int scores[TOTAL];
+    for (int i = 0; i < TOTAL; i ++)
+    {
+         scores[i] = get_int("Pontuação:");
+    }
+    // Imprimir média
+    printf ("Média: %d\n", (scores[0] + scores[1] + scores[2]) / TOTAL);
+}
