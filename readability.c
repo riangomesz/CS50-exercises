ad5a89740cdf3ee30eee5109f01d4f14c00f4819
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2023-02-01 13:21:21 -0300

    Add files via upload

---
+++ readability.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+int main (void)
+{
+    string text = get_string("Text: ");
+
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+
+        else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            sentences++;
+        }
+    }
+    float L = (float) letters / (float) words * 100;
+    float S = (float) sentences / (float) words * 100;
+
+    int index = 0.0588 * L - 0.296 * S - 15.8;
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n",index);
+    }
+}
