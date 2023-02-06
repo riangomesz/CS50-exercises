d02a09f8988a71f37f3aa08153373851ec2eed8d
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2023-02-06 14:04:43 -0300

    Add files via upload

---
+++ caesar.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key");
+        return 1;
+    }
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key");
+            return 1;
+        }
+    }
+        int k = atoi(argv[1]);
+
+        string plaintext = get_string("Plaintext: ");
+        printf("Ciphertext: ");
+
+        for (int j = 0; j < strlen(plaintext); j++)
+        {
+            if (isupper(plaintext[j]))
+            {
+                printf("%c",(plaintext[j] - 65 + k)% 26 + 65);
+            }
+
+            else if (islower(plaintext[j]))
+            {
+                printf("%c",(plaintext[j] - 97 + k) % 26 + 97);
+            }
+
+            else
+            {
+                printf("%c",plaintext[j]);
+            }
+        }
+        printf("\n");
+    }
