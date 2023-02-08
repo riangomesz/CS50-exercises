c0b4504d17cfdd9e1d6c0687277731745489dca1
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2023-02-08 10:53:07 -0300

    Add files via upload

---
+++ substitution.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+bool check_char(string key);
+
+int main(int argc, string argv[])
+{
+    string plaintext;
+    if (argc != 2) {
+        printf("Usage: /.substitution key\n");
+        return 1;
+    }
+    if (!check_char(argv[1])) {
+        printf("Key must contain 26 unique characters.\n");
+        return 1;
+    }
+    plaintext = get_string("plaintext: ");
+}
+
+bool check_char(string key)
+{
+    int lenght;
+    lenght = strlen(key);
+
+    if (lenght != 26) {
+        return false;
+    }
+    for (int i = 0; i < lenght; i++) {
+        key[i] = toupper(key[i]);
+    }
+    for (int i = 0; i < lenght; i++) {
+        if (!isalpha(key[i])) {
+            return false;
+        }
+        for (int j = i+1; j < lenght; j++) {
+            if (key[i] == key[j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
